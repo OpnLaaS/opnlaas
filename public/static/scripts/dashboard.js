e117a5ad7659b0a8fde6345b844dd8e8727d142c
codestutis <kestutisbiskis@gmail.com>
2025-11-02 21:38:16 -0500

    how to javascript?

--- public/static/scripts/dashboard.js
+++ public/static/scripts/dashboard.js
@@ -1,44 +1,79 @@
-const tbody = document.getElementById("host-table")
-const template = document.getElementById("host-row-template")
+const tbody = document.getElementById("hosts");
+const template = document.getElementById("host-row-template");
 
 function toggleRow(button) {
-      const tr = button.closest('tr');
-      const nextRow = tr.nextElementSibling;
-      const container = nextRow.querySelector('div');
-      const arrow = button.querySelector('svg');
-      const isCollapsed = container.classList.contains('max-h-0');
+  const tr = button.closest("tr");
+  const nextRow = tr.nextElementSibling;
+  const container = nextRow.querySelector("div");
+  const arrow = button.querySelector("svg");
+  const isCollapsed = container.classList.contains("max-h-0");
 
-      if (isCollapsed) {
-        container.classList.remove('max-h-0', 'opacity-0');
-        container.classList.add('max-h-40', 'opacity-100');
-        arrow.classList.add('rotate-180');
-      } else {
-        container.classList.add('max-h-0', 'opacity-0');
-        container.classList.remove('max-h-40', 'opacity-100');
-        arrow.classList.remove('rotate-180');
-      }
+  if (isCollapsed) {
+    container.classList.remove("max-h-0", "opacity-0");
+    container.classList.add("max-h-100", "opacity-100");
+    arrow.classList.add("rotate-180");
+  } else {
+    container.classList.add("max-h-0", "opacity-0");
+    container.classList.remove("max-h-100", "opacity-100");
+    arrow.classList.remove("rotate-180");
+  }
 }
 window.toggleRow = toggleRow;
 
-const users = [
-    { id: 1, name: 'thingy', power: 'on', ram: '8' },
-    { id: 2, name: 'other thingy', power: 'on', ram: '8' },
-    { id: 3, name: 'long name thingy more words', power: 'off', ram: '16' },
-    { id: 4, name: 'short', power: 'bruh', ram: '32' },
-    { id: 5, name: 'another thingy', power: 'power stuff', ram: '8' },
-    { id: 6, name: 'yet another thingy', power: 'on', ram: '4' },
-    { id: 7, name: 'stuff', power: 'on', ram: '64' },
-    { id: 8, name: 'more stuff', power: 'off', ram: '16' },
-    { id: 9, name: 'things', power: 'on', ram: '2' },
-    { id: 10, name: 'bruh', power: 'on', ram: '32' }
-];
+function reverseObject(obj) {
+    return Object.fromEntries(Object.entries(obj).map(([k, v]) => [v, k]))
+}
+
+async function getEnums(url, endpoint) {
+    const res = await fetch(`${url}/api/enums/${endpoint}`)
+    const data = await res.json();
+    return reverseObject(data)
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // i know evan said try/catch is bad but whatever
+  try {
+    // this might be somewhere else idk
+    const url = "http://localhost:8080";
+    const res = await fetch(`${url}/api/hosts`);
+    const data = await res.json();
+    // get enums
+    const vendorNames = await getEnums(url, 'vendors')
+    const form_factors = await getEnums(url, 'form-factors')
+    const mgmt_types = await getEnums(url, 'management-types')
+    const power_states = await getEnums(url, 'power-states')
 
 
-users.forEach(user => {
-    const clone = template.content.cloneNode(true);
-    clone.querySelector('[data-field="name"]').textContent = user.name;
-    clone.querySelector('[data-field="power"]').textContent = user.power;
-    clone.querySelector('[data-field="id"]').textContent = user.id;
-    clone.querySelector('[data-field="ram"]').textContent = user.ram;
-    tbody.appendChild(clone);
+    console.log(data);
+
+    data.forEach(host => {
+        const clone = template.content.cloneNode(true)
+        clone.querySelector('[data-field="form_factor"]').textContent = form_factors[host.form_factor]
+        clone.querySelector('[data-field="power"]').textContent = power_states[host.last_known_power_state]
+        clone.querySelector('[data-field="ip"]').textContent = host.management_ip
+        clone.querySelector('[data-field="mgmt-type"]').textContent = mgmt_types[host.management_type]
+        clone.querySelector('[data-field="name"]').textContent = host.model
+        // memory
+        clone.querySelector('[data-field="num_dimms"]').textContent = host.specs.memory.num_dimms
+        clone.querySelector('[data-field="size_gb"]').textContent = host.specs.memory.size_gb
+        clone.querySelector('[data-field="speed_mhz"]').textContent = host.specs.memory.speed_mhz
+        // processor
+        clone.querySelector('[data-field="cores"]').textContent = host.specs.processor.cores
+        clone.querySelector('[data-field="count"]').textContent = host.specs.processor.count
+        clone.querySelector('[data-field="sku"]').textContent = host.specs.processor.sku
+        clone.querySelector('[data-field="threads"]').textContent = host.specs.processor.threads
+        // storage
+        // TODO needs forEach or somethin, just the first one so far
+        clone.querySelector('[data-field="capacity_gb"]').textContent = host.specs.storage[0].capacity_gb
+        clone.querySelector('[data-field="media_type"]').textContent = host.specs.storage[0].media_type
+
+
+
+        clone.querySelector('[data-field="vendor"]').textContent = vendorNames[host.vendor]
+        // clone.querySelector('[data-field=""]').textContent = host.
+        tbody.appendChild(clone)
+    })
+  } catch (err) {
+    console.error(err);
+  }
 });
