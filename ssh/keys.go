2c44706e9dab84574828a1689b84e8411b074e39
z46-dev <evanparker6023@gmail.com>
2025-11-15 12:04:06 -0500

    Refactor CreateSSHKeyPair to use consistent file naming and improve cleanup logic

--- ssh/keys.go
+++ ssh/keys.go
@@ -5,7 +5,9 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 	"os"
+	"time"
 
 	"golang.org/x/crypto/ssh"
 )
@@ -21,17 +23,23 @@ func readFileHelper(path string) (data string, err error) {
 
 func CreateSSHKeyPair() (pub, priv string, err error) {
 	var (
-		privateKey     *rsa.PrivateKey
-		publicKey      ssh.PublicKey
-		pemBlock       *pem.Block
-		privateKeyFile *os.File
+		privateKey                *rsa.PrivateKey
+		publicKey                 ssh.PublicKey
+		pemBlock                  *pem.Block
+		privateKeyFile            *os.File
+		pubFileName, privFileName string = fmt.Sprintf("%s/opnlaas-ssh-keygen-%d.pub", os.TempDir(), time.Now().UnixNano()), fmt.Sprintf("%s/opnlaas-ssh-keygen-%d", os.TempDir(), time.Now().UnixNano())
 	)
+
+	defer func() {
+		os.Remove(pubFileName)
+		os.Remove(privFileName)
+	}()
 
 	if privateKey, err = rsa.GenerateKey(rand.Reader, 4096); err != nil {
 		return
 	}
 
-	if privateKeyFile, err = os.Create(os.TempDir() + "/id_rsa"); err != nil {
+	if privateKeyFile, err = os.Create(privFileName); err != nil {
 		return
 	}
 
@@ -54,15 +62,15 @@ func CreateSSHKeyPair() (pub, priv string, err error) {
 		return
 	}
 
-	if err = os.WriteFile(os.TempDir()+"/id_rsa.pub", ssh.MarshalAuthorizedKey(publicKey), 0644); err != nil {
+	if err = os.WriteFile(pubFileName, ssh.MarshalAuthorizedKey(publicKey), 0644); err != nil {
 		return
 	}
 
-	if pub, err = readFileHelper(os.TempDir() + "/id_rsa.pub"); err != nil {
+	if pub, err = readFileHelper(pubFileName); err != nil {
 		return
 	}
 
-	if priv, err = readFileHelper(os.TempDir() + "/id_rsa"); err != nil {
+	if priv, err = readFileHelper(privFileName); err != nil {
 		return
 	}
 
